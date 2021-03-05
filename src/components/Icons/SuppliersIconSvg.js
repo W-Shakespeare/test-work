bb89b285a8def7d5b5baf3da6f84a83f4ef15732
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2021-03-05 07:47:56 +0200

    Created Icons directory

---
+++ src/components/Icons/SuppliersIconSvg.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+export const SuppliersIconSvg = () => {
+  return (
+    <div>
+      <div className="suppliers-svgDiv1">
+        <svg
+          width="16"
+          height="7"
+          viewBox="0 0 16 7"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 2.88357V2.87957L12.994 4.22057L16 3.09357L8 0.0935669L0 3.09357L8 6.09357L10.994 4.97057L8 3.73657V3.73357L10 2.88357Z"
+            fill="white"
+            fill-opacity="0.6"
+          />
+        </svg>
+      </div>
+      <div className="suppliers-svgDiv2">
+        <svg
+          width="8"
+          height="12"
+          viewBox="0 0 8 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 0.0935669V9.09357L7.5 11.9066V2.90657L0 0.0935669ZM4 9.12657L2 8.37657V7.30957L4 8.05957V9.12657Z"
+            fill="white"
+            fill-opacity="0.6"
+          />
+        </svg>
+      </div>
+      <div className="suppliers-svgDiv3">
+        <svg
+          width="8"
+          height="12"
+          viewBox="0 0 8 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M5 1.21857V3.72357L3 4.47357V1.96857L0.5 2.90657V11.9066L8 9.09357V0.0935669L5 1.21857Z"
+            fill="white"
+            fill-opacity="0.6"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Icons/index.js
@@ -0,0 +1,7 @@
+export { FinanceIconSvg } from "./FinanceIconSvg";
+export { FlimcorIconSvg } from "./FlimcorIconSvg";
+export { MenuIconSvg } from "./MenuIconSvg";
+export { OrderIconSvg } from "./OrderIconSvg";
+export { SuppliersIconSvg } from "./SuppliersIconSvg";
+export { UsersIconSvg } from "./UsersIconSvg";
+export { UsersOrderIconSvg } from "./UsersOrderIconSvg";
